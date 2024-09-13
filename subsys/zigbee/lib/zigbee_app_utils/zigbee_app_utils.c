827f1bf8af13ac9d3b7a12ad67d96d0166e08a52
Eduardo Montoya <eduardo.montoya@nordicsemi.no>
2024-09-13 12:47:31 +0200

    zigbee: remove `CONFIG_ZIGBEE_PANID_CONFLICT_RESOLUTION`
    In R23 it is not possible to disable PANID conflict detection.
    
    Signed-off-by: Eduardo Montoya <eduardo.montoya@nordicsemi.no>

--- subsys/zigbee/lib/zigbee_app_utils/zigbee_app_utils.c
+++ subsys/zigbee/lib/zigbee_app_utils/zigbee_app_utils.c
@@ -253,10 +253,6 @@ zb_ret_t zigbee_default_signal_handler(zb_bufid_t bufid)
 		ZB_ERROR_CHECK(zb_zcl_set_backward_comp_mode(ZB_ZCL_AUTO_MODE));
 		ZB_ERROR_CHECK(
 			zb_zcl_set_backward_compatible_statuses_mode(ZB_ZCL_STATUSES_ZCL8_MODE));
-		if (IS_ENABLED(CONFIG_ZIGBEE_PANID_CONFLICT_RESOLUTION)) {
-			zb_enable_panid_conflict_resolution(
-				CONFIG_ZIGBEE_PANID_CONFLICT_RESOLUTION);
-		}
 		stack_initialised = true;
 		LOG_INF("Zigbee stack initialized");
 		comm_status = bdb_start_top_level_commissioning(
